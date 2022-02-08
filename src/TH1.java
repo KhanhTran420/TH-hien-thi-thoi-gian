75ea8c590a2d311ee12bf7611c7460e11a887cd9
unknown <khanhbi1998pt@gmail.com>
2022-02-08 11:11:07 +0700

    TH hien thi time

---
+++ src/TH1.java
@@ -0,0 +1,10 @@
+
+import java.util.Date;
+
+public class TH1 {
+    public static void main(String[] args) {
+        Date now = new Date();   //Lay ra thoi gian hien tai
+
+        System.out.println("now is " + now);
+    }
+}
